361f0006a52d3c80cf7ff2f7021f59f4e8544cd9
AbdessamadPas <boughadoin@gmail.com>
2023-03-19 22:41:48 +0100

    wewe

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
 
-function App() {
+const proxyUrl = 'https://cors-anywhere.herokuapp.com/'
+const stonksUrl = `${proxyUrl}https://query1.finance.yahoo.com/v8/finance/chart/GME?region=US&lang=en-US&includePrePost=false&interval=15m&range=5d&corsDomain=search.yahoo.com`
+const getStonks = async()=>{
+  const response = await fetch(stonksUrl)
+  return response.json()
+}
+
+function App() { 
+  const [price, setPrice] = React.useState(0)
+  const [priceTime, setPriceTime] = React.useState(null)
+  React.useEffect(() => {
+    let timeoutId
+    const getLatestPrice= async()=>{
+      const data = await getStonks()
+      const meta = data.chart.result[0].meta
+      setPrice(meta.regularMarketPrice)
+      setPriceTime(new Date(meta.regularMarketTime * 1000))
+      timeoutId = setTimeout(getLatestPrice, 5000) 
+    }
+    setTimeout(getLatestPrice, 5000 * 2) 
+    return ()=>{
+      clearTimeout(timeoutId)
+    }
+    
+  })
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className="price">
+      ${price}
+     
     </div>
+     {priceTime && priceTime.toLocaleTimeString()}
+    </>
+    
   );
 }
 
